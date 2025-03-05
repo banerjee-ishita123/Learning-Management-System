cd50823ad0067da150ab3fabd7f68247b00f32b2
banerjee-ishita123 <banerjeeishita2005@gmail.com>
2025-03-05 11:24:56 +0530

    Course List page added

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -3,18 +3,35 @@ import { assets } from '../../assets/assets'
 
 const Footer = () => {
   return (
-    <Footer className='bg-gray-900 md:px-10 text-left w-full mt-10'>
+    <footer className='bg-gray-900 md:px-10 text-left w-full mt-10'>
       <div className='flex flex-col md:flex-row items-start px-8 md:px-0 justify-center gap-10 md:gap-32 py-10 border-b border-white/30'>
-        <div className='flex flex-col md:items-start items-center w-full'><img src={assets.logo_dark} alt='logo'></img>
-        <p>Learn anything, anytime, anywhere. Expand your knowledge and skills with ease through interactive courses, 
+        <div className='flex flex-col md:items-start items-center w-full'> <img src={assets.logo_dark} alt='logo'></img>
+        <p className='mt-6 text-center md:text-left text-sm text-white/80 '>Learn anything, anytime, anywhere. Expand your knowledge and skills with ease through interactive courses, 
           expert guidance, and a seamless learning experience. </p>
         </div>
         
-        <div></div>
-        <div></div>
+        <div className='flex flex-col md:items-start ites-center w-full'>
+          <h2 className='font-semibold text-white mb-5'>Companies</h2>
+          <ul className='flex md:flex-col w-full justify-between text-sm text-white/80 md:space-y-2'>
+
+            <li><a href='#'>Home</a></li>
+            <li><a href='#'>About us</a></li>
+            <li><a href='#'>Contact us</a></li>
+            <li><a href='#'>Privacy Policy</a></li>
+          </ul>
+        </div>
+        <div className='hidden md:flex flex-col items-start w-full'>
+          <h2 className='font-semibold text-white mb-5'>Subscribe to our newsletter</h2>
+          <p className='text-sm text-white/60'>The latest news,artical,and resourses send to 
+          <br/> your inbox weekly</p>
+          <div>
+            <input type='email' placeholder='Enter your email' className='border border-gray-500/30 bg-gray-800 text-gray-500 placeholder-gray-500 outline-none h-9 w-64 rounded px-2 text-sm'/>
+            <button className='bg-blue-600 w-24 h-9 text-white rounded ml-3'> Subscribe</button>
+          </div>
+        </div>
      </div>
-     <p>Copyright 2025 © Ishita Banerjee. All Right Reserved.</p>
-    </Footer>
+     <p className='py-4 text-center text-xs md:text-sm text-white/60'>Copyright 2025 © Ishita Banerjee. All Right Reserved.</p>
+    </footer>
   )
 }
 
--- client/src/pages/student/Home.jsx
+++ client/src/pages/student/Home.jsx
@@ -6,6 +6,8 @@ import TestimonialsSection from '../../components/student/TestimonialsSection'
 import CallToAction from '../../components/student/CallToAction'
 import Footer from '../../components/student/Footer'
 
+
+
 const Home = () => {
   return (
     <div className='flex flex-col items-center space-y-7 text-center'>
