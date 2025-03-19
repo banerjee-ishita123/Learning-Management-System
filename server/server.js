93cd81ac4d23de9f07da036f543a9047d0823c7d
banerjee-ishita123 <banerjeeishita2005@gmail.com>
2025-03-19 09:44:53 +0530

    server updated

--- server/server.js
+++ server/server.js
@@ -26,7 +26,8 @@ app.post('/clerk',express.json(),clerkWebhooks)
 app.use('/api/educator',express.json(),educatorRouter)
 app.use('/api/course',express.json(),courseRouter)
 app.use('/api/user',express.json(),userRouter)
-app.post('/stipe',express.raw({type:'application/json'}),stripeWebhooks)
+app.post('/stripe', express.raw({ type: 'application/json' }), stripeWebhooks);
+
 //port
 const PORT=process.env.PORT || 5000
 
