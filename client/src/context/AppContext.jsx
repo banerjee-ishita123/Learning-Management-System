6cec8769968cc801d3e273753b286ffe063c8fb4
banerjee-ishita123 <146516084+banerjee-ishita123@users.noreply.github.com>
2025-03-21 20:52:00 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -2,11 +2,14 @@ import { createContext, useEffect, useState } from "react";
 import { dummyCourses } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import humanizeDuration from 'humanize-duration';
-import {useAuth,useUser} from '@clerk/clerk-react'
+import {useAuth,useUser} from '@clerk/clerk-react';
+import axios from 'axios';
+
+import {  toast } from 'react-toastify';
 export const AppContext = createContext()/* AppContex act like a storage box where we can keep storage data*/ 
 export const AppContextProvider=(props)=>{
   /* This AppContexProvider component will wrap around the parts of our app that need access to the shared data.*/
-  
+  const backendUrl=import.meta.env.VITE_BACKEND_URL
   const currency=import.meta.env.VITE_CURRENCY
   const navigate=useNavigate()
   const {getToken}=useAuth()
@@ -15,24 +18,56 @@ export const AppContextProvider=(props)=>{
 
 
   const [allCourses,setallCourses]=useState([])
-  const [isEducator,setisEducator]=useState(true)
-  const [enrollCourses,setenrollCourses]=useState([])
+  const [isEducator,setIsEducator]=useState(false)
+  const [enrolledCourses,setEnrolledCourses]=useState([])
+  const [userData,setUserData]=useState(null)
+  const [loading, setLoading] = useState(true);
 
   //Fetch all courses
   const fetchAllCourses= async ()=>{
-    setallCourses(dummyCourses)
+   try {
+    const {data}=await axios.get(backendUrl + '/api/course/all')
+    if(data.success){
+      setallCourses(data.courses)
+      
+    }else{
+      toast.error(data.message)
+    }
+   } catch (error) {
+    toast.error(error.message)
+   }
+  }
+  //fetch user
+  const fetchUserData=async()=>{
+    if (user?.publicMetadata?.role === 'educator') {
+      setIsEducator(true);
+  }
+    try {
+      const token=await getToken();
+    console.log(token)
+     const {data}=await axios.get(backendUrl + '/api/user/data',{headers: {Authorization:`Bearer:${token}`}})
+      
+      
+      if(data.success){
+        setUserData(data.user)
+      }else{
+        toast.error(data.message)
+      }
+    } catch (error) {
+      toast.error(error.message)
+    }
   }
   //function to calculate avearage rating course
-  const calculateRating=(course)=>{
-    if (course.courseRatings.length===0){
-      return 0
+  const calculateRating = (course) => {
+    if (!course || !Array.isArray(course.courseRatings) || course.courseRatings.length === 0) {
+      return 0; // Return 0 if course or courseRatings is undefined or empty
     }
-    let totalRating=0
-    course.courseRatings.forEach(rating=>{
-      totalRating+=rating.rating
-    })
-    return totalRating / course.courseRatings.length
-  }
+  
+    const totalRating = course.courseRatings.reduce((sum, rating) => sum + (rating.rating || 0), 0);
+    
+    return totalRating / course.courseRatings.length;
+  };
+  
   //funcion to calculate course chapter time
   const calculChapterTime =(chapter)=>{
     let time=0
@@ -59,24 +94,40 @@ export const AppContextProvider=(props)=>{
   };
   //fetch User Enrolled courses
   const fetchEnrolledCourses=async ()=>{
-    setenrollCourses(dummyCourses)
+    try {
+      const token=await getToken()
+    const {data}=await axios.get(backendUrl +'/api/user/enrolled-courses',{headers:{Authorization:`Bearer:${token}`}})
+    if(data.success){
+      setEnrolledCourses(data.enrolledCourses.reverse())
+    }else{
+      toast.error(data.message)
+    }
+    } catch (error) {
+      toast.error(error.message)
+    }
+    
   }
   
   useEffect(()=>{
    fetchAllCourses()
-   fetchEnrolledCourses()
+  
   },[])
 
-  const logTokan=async()=>{
-    console.log(await getToken());
-  }
-  useEffect(()=>{
-if(user){
-logTokan()
-}
-  },[user])
+ 
+  useEffect(() => {
+    if (user) {
+        Promise.all([fetchUserData(), fetchEnrolledCourses()]).then(()=>{
+            setLoading(false)
+        });
+        if (user?.publicMetadata?.role === 'educator') {
+            setIsEducator(true);
+        }
+    }else{
+        setLoading(false)
+    }
+}, [user]);
     const value={
-       currency,allCourses,navigate,calculateRating,isEducator,setisEducator,calculChapterTime,calculateCourseDuration,calculateNoOfLecture,enrollCourses,fetchEnrolledCourses
+       currency,allCourses,navigate,calculateRating,isEducator,setIsEducator,calculChapterTime,calculateCourseDuration,calculateNoOfLecture,enrolledCourses,fetchEnrolledCourses,backendUrl,setUserData,userData,getToken,fetchAllCourses
     }
         return(
           /*<AppContext.Provider> is the actual provider that shares data. 
